8d4c3b807d7684d3a86e759ed7663ce045505830
Liam Collins <jeliroco333@gmail.com>
2023-03-29 08:39:13 -0400

    footer updated with reddit and discord

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,8 +11,7 @@ const Footer: React.FC<FooterProps> = ({}) => {
       if (document.body.scrollTop + document.documentElement.scrollTop !== 0) {
         FooterRef.current.classList.remove("translate-y-14");
         FooterRef.current.classList.remove("opacity-0");
-      }
-      else {
+      } else {
         FooterRef.current.classList.add("translate-y-14");
         FooterRef.current.classList.add("opacity-0");
       }
@@ -27,8 +26,19 @@ const Footer: React.FC<FooterProps> = ({}) => {
 
   return (
     <>
-      <footer ref={FooterRef} className="fixed bottom-0 left-0 right-0 z-[1000] flex p-1 duration-300 ease-in-out bg-orange-400 border-t-2 border-orange-500 opacity-0 translate-y-14">
-        <div className="flex-1 text-center">copyright 2023 by jeliroco. All rights reserved.</div>
+      <footer
+        ref={FooterRef}
+        className="fixed bottom-0 left-0 right-0 z-[1000] flex gap-2 p-1 duration-300 ease-in-out bg-orange-400 border-t-2 border-orange-500 opacity-0 translate-y-14"
+      >
+        <div className="text-xs text-left text-white underline flex-0">
+          <a target="_blank" href="https://reddit.com/r/sngr">reddit</a>
+        </div>
+        <div className="text-xs text-left text-white underline flex-0">
+          <a target="_blank" href="https://discord.gg/FvumZprMcE">discord</a>
+        </div>
+        <div className="flex-1 text-xs text-right text-white">
+          © 2023 jeliroco multimedia. All rights reserved.
+        </div>
       </footer>
       <div className="h-10"></div>
     </>
