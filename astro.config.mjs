13d5b4b0b7bb23fd77c60ba15c332f8e02bc6b92
Liam Collins <jeliroco333@gmail.com>
2023-03-30 23:10:59 -0400

    trying vercelServerless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 import image from "@astrojs/image";
 
@@ -9,5 +9,5 @@ import image from "@astrojs/image";
 export default defineConfig({
   integrations: [tailwind(), react(), image()],
   output: "server",
-  adapter: vercel()
+  adapter: vercelServerless()
 });
